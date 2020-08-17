9bb14c5be890acc9f8afd91fbbfe0ac265b5dc57
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-17 20:09:43 -0300

    trabalhando com listas

---
+++ 6-listas.js
@@ -0,0 +1,17 @@
+console.log(`Trabalhando com listas`);
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+// trabalhando com Arrays
+
+const listaDeDestinos = new Array(
+    `Salvador`, `São Paulo`, `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`) // adicionando um item na lista
+
+console.log("Possíveis destinos:");
+//console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listaDeDestinos);
