b4940e5c5ff24e927cbfa18ec6a6d58703b6d9a4
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-17 18:22:52 -0300

    trabalhando com variáveis

--- 1-meuPrimeiroProgama.js
+++ 1-meuPrimeiroProgama.js
@@ -1,1 +1,11 @@
-console.log("Meu primeiro progama");
+console.log("Meu primeiro progama: trabalhando com variáveis");
+
+const idade = 33;
+
+console.log("Minha idade é", idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
