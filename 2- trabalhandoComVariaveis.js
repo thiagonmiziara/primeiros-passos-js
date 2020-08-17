e2091cce557637091ce9b9e952d76f84db9edd8d
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-17 19:57:32 -0300

    adcionando mais arquivos js

---
+++ 2- trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com variáveis");
+//Js é case sensitive
+
+const idade = 33;
+const nome = "Thiago";
+let ano = 2020;
+console.log(nome, idade, ano);
+
+// não fazer
+//mes = "Janeiro";
---
+++ 5-atribuicaoDeVariaves.js
@@ -0,0 +1,23 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+
+//const idade = 33;
+const primeiroNome = "Thiago";
+const sobrenome = "Nunes Miziara";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+// sobescrevendo a variavel
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let contador = 0;
+contador = contador + 1;
+
+let idade; // declarando variável
+idade = 33; // atribuindo valor
+idade = idade + 1;
+console.log(idade);
